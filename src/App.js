ec9cff73f0abaa8962d1c38f4094a610cafbeaaa
rontrias7 <rontrias7@gmail.com>
2022-11-11 15:46:00 -0300

    minor UI changes

--- src/App.js
+++ src/App.js
@@ -119,9 +119,9 @@ const Display = ({results: {bad, good, excellent}}) =>{
   return(
   <>
 
-    <p><IconContext.Provider value={{ className: "no-pointer bad-dot", size: "0.7em" }}><GiPlainCircle /></IconContext.Provider> Bads <br /> <span className="numbers">{bad}</span> <br/>
-    <IconContext.Provider value={{ className: "no-pointer good-dot", size: "0.7em" }}><GiPlainCircle /></IconContext.Provider> Goods <br /> <span className="numbers">{good}</span><br/>
-    <IconContext.Provider value={{ className: "no-pointer excellent-dot", size: "0.7em" }}><GiPlainCircle /></IconContext.Provider> Excellent <br /> <span className="numbers">{excellent}</span> </p>
+    <p><IconContext.Provider value={{ className: "no-pointer bad-dot", size: "0.7em" }}><GiPlainCircle /></IconContext.Provider> Bads <br /> <span className="numbers">{bad}</span> <br/> <br/>
+    <IconContext.Provider value={{ className: "no-pointer good-dot", size: "0.7em" }}><GiPlainCircle /></IconContext.Provider> Goods <br /> <span className="numbers">{good}</span><br/> <br/>
+    <IconContext.Provider value={{ className: "no-pointer excellent-dot", size: "0.7em" }}><GiPlainCircle /></IconContext.Provider> Excellent <br /> <span className="numbers">{excellent}</span> <br/><br/></p>
 
   </>
 
