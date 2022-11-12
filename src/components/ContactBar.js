e65819184ce4fed40e630275e7262188a56487af
rontrias7 <rontrias7@gmail.com>
2022-11-11 22:20:25 -0300

    some UI changes

--- src/components/ContactBar.js
+++ src/components/ContactBar.js
@@ -10,10 +10,10 @@ let ContactBar = () => {
       <div className="contact-bar">
       <span>Let's talk </span>
       <IconContext.Provider value={{ className: "default", size: "1.5em", margin: "0px 8px", color: 'black' }}>
-      <a href="https://github.com/rontrias7" rel="noreferrer" target="_blank"><FaGithub /></a>
+      <a href="mailto:rontrias7@gmail.com" rel="noreferrer" target="_blank"><CgMail /></a>
       </IconContext.Provider>
       <IconContext.Provider value={{ className: "default", size: "1.5em", margin: "0px 8px", color: 'black' }}>
-      <a href="mailto:rontrias7@gmail.com" rel="noreferrer" target="_blank"><CgMail /></a>
+      <a href="https://github.com/rontrias7" rel="noreferrer" target="_blank"><FaGithub /></a>
       </IconContext.Provider>
       </div>
 
